020b05e632269536396452eeb532db9cbc47570a
judee7733 <judeputhussery360@gmail.com>
2024-05-26 18:05:01 +0530

    edited whichkey, telescope(keymaps and dependencies)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,25 +2,29 @@ local mapkey = require("util.keymapper").mapkey
 
 local config = function()
 	local telescope = require("telescope")
+  local actions = require("telescope.actions")
+
 	telescope.setup({
 		defaults = {
-			mappings = {
-				i = {
-					["<C-j>"] = "move_selection_next",
-					["<C-k>"] = "move_selection_previous",
-				},
-			},
-		},
+      path_display = { "smart" },
+      mappings = {
+        i = {
+          ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+          ["<C-j>"] = actions.move_selection_next, -- move to next result
+          -- ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+        },
+      },
+	  },
 		pickers = {
 			find_files = {
 				theme = "dropdown",
 				previewer = false,
 				hidden = true,
 			},
-			live_grep = {
-				theme = "dropdown",
-				previewer = false,
-			},
+			-- live_grep = {
+			-- 	theme = "dropdown",
+			-- 	previewer = false,
+			-- },
 			buffers = {
 				theme = "dropdown",
 				previewer = false,
@@ -31,13 +35,18 @@ end
 
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.3",
+  branch = "0.1.x",
 	lazy = false,
-	dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
+  },
 	config = config,
 	keys = {
 		mapkey("<leader>fk", "Telescope keymaps", "n"),
 		mapkey("<leader>fh", "Telescope help_tags", "n"),
+    mapkey("<leader>fr", "Telescope oldfiles", "n"),
 		mapkey("<leader>ff", "Telescope find_files", "n"),
 		mapkey("<leader>fg", "Telescope live_grep", "n"),
 		mapkey("<leader>fb", "Telescope buffers", "n"),
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,3 +1,8 @@
 return {
+  event = "VeryLazy",
 	"folke/which-key.nvim",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 500
+  end,
 }

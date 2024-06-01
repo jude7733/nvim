b97ccf6e9afd021581ad97b6cb9f341d173908f2
jude7733 <judeputhussery360@gmail.com>
2024-06-01 19:36:05 +0530

    fixed some issues
    changed some keybinding

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,10 +1,10 @@
 return {
-  "rmagatti/auto-session",
-  lazy = false,
-  config = function()
-    require("auto-session").setup({
-      auto_restore_enabled = false,
-      auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop/" },
-    })
-  end,
+	"rmagatti/auto-session",
+	lazy = false,
+	config = function()
+		require("auto-session").setup({
+			auto_restore_enabled = false,
+			auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop/" },
+		})
+	end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,44 +1,42 @@
 local config = function()
-  local telescope = require("telescope")
-  local actions = require("telescope.actions")
+	local telescope = require("telescope")
+	local actions = require("telescope.actions")
+	local open_with_trouble = require("trouble.sources.telescope").open
 
-  telescope.setup({
-    defaults = {
-      path_display = { "smart" },
-      mappings = {
-        i = {
-          ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-          ["<C-j>"] = actions.move_selection_next,     -- move to next result
-          -- ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-        },
-      },
-    },
-    pickers = {
-      find_files = {
-        theme = "dropdown",
-        previewer = false,
-        hidden = true,
-      },
-      -- live_grep = {
-      -- 	theme = "dropdown",
-      -- 	previewer = false,
-      -- },
-      buffers = {
-        theme = "dropdown",
-        previewer = false,
-      },
-    },
-  })
+	telescope.setup({
+		defaults = {
+			path_display = { "smart" },
+			mappings = {
+				i = {
+					["<C-k>"] = actions.move_selection_previous,
+					["<C-j>"] = actions.move_selection_next,
+					["<c-t>"] = open_with_trouble,
+				},
+				n = { ["<c-t>"] = open_with_trouble },
+			},
+		},
+		pickers = {
+			find_files = {
+				theme = "dropdown",
+				previewer = false,
+				hidden = true,
+			},
+			buffers = {
+				theme = "dropdown",
+				previewer = false,
+			},
+		},
+	})
 end
 
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  lazy = false,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = config,
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	lazy = false,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = config,
 }

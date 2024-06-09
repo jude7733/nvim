b27016b5abc5cf8de0537ff25dc971e901cc19bd
jude7733 <judeputhussery360@gmail.com>
2024-06-09 23:38:58 +0530

    fix autosession nvim-tree bug

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,9 +1,7 @@
 return {
 	"rmagatti/auto-session",
-	lazy = false,
 	config = function()
 		require("auto-session").setup({
-			auto_restore_enabled = false,
 			auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop/" },
 		})
 	end,
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,46 +1,55 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  lazy = false,
-  config = function()
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+	"nvim-tree/nvim-tree.lua",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-    require("nvim-tree").setup({
-      view = {
-        width = 28,
-      },
-      -- change folder arrow icons
-      renderer = {
-        indent_markers = {
-          enable = true,
-        },
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
-            },
-          },
-        },
-      },
-      -- disable window_picker for
-      -- explorer to work well with
-      -- window splits
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = false,
-          },
-        },
-      },
-      git = {
-        ignore = false,
-      },
-      filters = {
-        dotfiles = false,
-        custom = { ".DS_Store" },
-      },
-    })
-  end,
+		require("nvim-tree").setup({
+			view = {
+				width = 28,
+			},
+			-- change folder arrow icons
+			renderer = {
+				indent_markers = {
+					enable = true,
+				},
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "", -- arrow when folder is closed
+							arrow_open = "", -- arrow when folder is open
+						},
+					},
+				},
+			},
+			-- disable window_picker explorer to work well window splits
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+			git = {
+				ignore = false,
+			},
+			filters = {
+				dotfiles = false,
+				custom = { ".DS_Store" },
+			},
+		})
+		vim.api.nvim_create_autocmd({ "BufEnter" }, {
+			pattern = "NvimTree*",
+			callback = function()
+				local view = require("nvim-tree.view")
+				local is_visible = view.is_visible()
+
+				local api = require("nvim-tree.api")
+				if not is_visible then
+					api.tree.open()
+				end
+			end,
+		})
+	end,
 }

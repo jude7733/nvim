f32e013b3e684ee92b2eea0c49f4ef1b2c8c2ad7
jude7733 <judeputhussery360@gmail.com>
2024-06-11 00:12:08 +0530

    auto close nvim tree
    updated packages

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,46 +1,44 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  lazy = false,
-  config = function()
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+	"nvim-tree/nvim-tree.lua",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	lazy = false,
+	config = function()
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-    require("nvim-tree").setup({
-      view = {
-        width = 28,
-      },
-      -- change folder arrow icons
-      renderer = {
-        indent_markers = {
-          enable = true,
-        },
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
-            },
-          },
-        },
-      },
-      -- disable window_picker for
-      -- explorer to work well with
-      -- window splits
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = false,
-          },
-        },
-      },
-      git = {
-        ignore = false,
-      },
-      filters = {
-        dotfiles = false,
-        custom = { ".DS_Store" },
-      },
-    })
-  end,
+		require("nvim-tree").setup({
+			view = {
+				width = 28,
+			},
+			-- change folder arrow icons
+			renderer = {
+				indent_markers = {
+					enable = true,
+				},
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "", -- arrow when folder is closed
+							arrow_open = "", -- arrow when folder is open
+						},
+					},
+				},
+			},
+			actions = {
+				open_file = {
+					quit_on_open = true,
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+			git = {
+				ignore = false,
+			},
+			filters = {
+				dotfiles = false,
+				custom = { ".DS_Store" },
+			},
+		})
+	end,
 }

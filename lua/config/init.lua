5fd0b52759df2ae0ff05f8e79e4bcd1a589d5a1e
jude7733 <judeputhussery360@gmail.com>
2024-07-28 17:39:02 +0530

    removed highlighed-yank, added markdown plugin, autocommands, nvim-cpm sources
    implemented yank highlighting with autocommands
    updated plugins

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-require('config.lazy')
-require('config.options')
-require('config.keymaps')
+require("config.lazy")
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
--- lua/plugins/nvim-highlighted-yank.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "machakann/vim-highlightedyank",
-  event = "VeryLazy",
-}

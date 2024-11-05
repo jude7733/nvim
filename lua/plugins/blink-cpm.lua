a23c13aac357020ac7190927c3c0cf558fce9a25
jude7733 <judeputhussery360@gmail.com>
2024-11-05 12:08:23 +0530

    lsp support for blink.cmp

--- lua/plugins/blink-cpm.lua
+++ lua/plugins/blink-cpm.lua
@@ -5,15 +5,26 @@ return {
 	version = "v0.*",
 	opts = {
 		keymap = {
-			hide = "<C-d>",
-			accept = "<CR>",
-			select_prev = { "<Up>", "<C-k>" },
-			select_next = { "<Down>", "<C-j>" },
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-h>"] = { "hide" },
+			["<C-CR>"] = { "select_and_accept" },
+
+			["<C-k>"] = { "select_prev", "fallback" },
+			["<C-j>"] = { "select_next", "fallback" },
+
+			["<C-u>"] = { "scroll_documentation_up", "fallback" },
+			["<C-d>"] = { "scroll_documentation_down", "fallback" },
+
+			["<Tab>"] = { "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
 		},
 		highlight = {
 			use_nvim_cmp_as_default = true,
 		},
 		trigger = { signature_help = { enabled = true } },
 		accept = { auto_brackets = { enabled = true } },
+		documentation = {
+			auto_show = true,
+		},
 	},
 }
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,1 +1,10 @@
-return { "hrsh7th/nvim-cmp" }
+return {
+	"hrsh7th/nvim-cmp",
+	config = function()
+		require("cmp").setup({
+			completion = {
+				autocomplete = false,
+			},
+		})
+	end,
+}
